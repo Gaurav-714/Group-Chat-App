a9dc96b0b7a76dd4ecc106041e8a56620d024e8c
Gaurav-714 <gauravnagose714@gmail.com>
2025-02-23 23:32:23 +0530

    Authentication Applied

--- home/consumers.py
+++ home/consumers.py
@@ -4,43 +4,51 @@ from channels.db import database_sync_to_async
 import json
 from .models import GroupModel, ChatModel
 
-
 class ChatConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         print(f"✅ Websocket Connected: {self.channel_name}")
 
         self.group_name = self.scope['url_route']['kwargs']['group_name']
         await self.channel_layer.group_add(self.group_name, self.channel_name)
-        await self.send({
-            'type': 'websocket.accept'
-        })
+        await self.send({'type': 'websocket.accept'})
 
     async def websocket_receive(self, event):
-        print(f"📩 Message Received From Client: {event['text']}")
+        #print(f"📩 Message Received From Client: {event['text']}")
         try:
             data = json.loads(event['text'])
             group = await database_sync_to_async(GroupModel.objects.get)(name=self.group_name)
-            chat = await database_sync_to_async(ChatModel.objects.create)(
-                content=data['message'], group=group
-            )
-            # Send message to group
-            await self.channel_layer.group_send(
-                self.group_name, {
-                    'type': 'chat.message',
-                    'message': data['message']  # Extracted from JSON
-                }
-            )
+
+            if self.scope['user'].is_authenticated:
+                chat = await database_sync_to_async(ChatModel.objects.create)(
+                    content=data['message'], group=group, sender=self.scope['user']
+                )
+                await self.channel_layer.group_send(
+                    self.group_name, {
+                        'type': 'chat.message',
+                        'message': data['message'],
+                        'sender': self.scope['user'].username
+                    }
+                )
+            else:
+                await self.send({
+                    'type': 'websocket.send',
+                    'text': json.dumps({"message": "Login Required"})  
+                })
+
         except json.JSONDecodeError:
             print("❌ Invalid JSON received.")
         except GroupModel.DoesNotExist:
             print(f"❌ Group '{self.group_name}' does not exist.")
 
     async def chat_message(self, event):
-        print(f"🔄 Broadcasting Message: {event['message']}")
+        #print(f"🔄 Broadcasting Message: {event['message']}")
 
         await self.send({
             'type': 'websocket.send',
-            'text': event['message']
+            'text': json.dumps({
+                "message": event['message'],
+                "sender": event['sender']
+            })  
         })
 
     async def websocket_disconnect(self, event):
--- home/models.py
+++ home/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class GroupModel(models.Model):
     name = models.CharField(max_length=255)
@@ -15,10 +17,11 @@ class ChatModel(models.Model):
     content = models.CharField(max_length=1000)
     timestamp = models.DateField(auto_now=True)
     group = models.ForeignKey(GroupModel, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Chat'
         verbose_name_plural = 'Chats'
 
     def __str__(self):
-        return self.content
+        return f"{self.content} - {self.timestamp}"
